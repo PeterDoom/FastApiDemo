1bedd7883e506051f52d36d66977e060bc8ac209
PeterDoom <48176167+PeterDoom@users.noreply.github.com>
2021-12-01 21:38:16 +0200

    adding random error throwing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import datetime
+import http
+import random
+import uuid
+
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
-@app.get("/")
-async def welcome_page():
-    return "Hello World"
+@app.get("/info/generate/id")
+async def generate_id():
+    rand_int = random.Random().randint(0, 10000)
+
+    if rand_int >= 5121:
+        return {"dateOfExecution": datetime.datetime.utcnow(), "id": uuid.uuid4()}
+    elif rand_int <= 5121:
+        raise HTTPException(status_code=500, detail="connection failed")
